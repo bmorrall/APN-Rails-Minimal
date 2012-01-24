f43de3a5bebff138793a8e051b8fe2a9b0d4f13c
bmorrall <ben.morrall@sitepoint.com>
2012-01-24 11:03:36 +1100

    Created Initial Rails App

---
+++ apn-webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApnWebapp::Application.config.secret_token = 'bcdeaceea996cddf2e750b583872535a7cc620916c9d749c809019bcd1bdc15ee028c2de86efd0bd0ffcbfb8141779598254c5398d658b85771451e70a12ca01'
